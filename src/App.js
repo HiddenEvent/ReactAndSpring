8fd18e89fe8334fb160de15543e03ec23982fba6
RichardKim <hiddeneventor@gmail.com>
2021-03-18 11:45:05 +0900

    React 기초 공부중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 0. React 엔진 - 데이터변경감지해서 UI 그려주는!!
+// 1. 실행 과정 (index.html) - SPA, index.html에서 body만 바꾸는 기술, 페이지 이동이 아니다.
+// 2. JSX 문법 = javascript에서 html을 사용가능하게 하는 문법
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>안녕2</div>;
 }
 
 export default App;
