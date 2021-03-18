db9b019984ddca534899704375f6b4c747190985
RichardKim <hiddeneventor@gmail.com>
2021-03-18 16:32:40 +0900

    생명주기 및 상태값 변경, import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Sub from './Sub';
 
 // 0. React 엔진 - 데이터변경감지해서 UI 그려주는!!
 // 1. 실행 과정 (index.html) - SPA, index.html에서 body만 바꾸는 기술, 페이지 이동이 아니다.
@@ -11,13 +13,19 @@ import './App.css';
 // 4) css 디자인 - 외부파일에 정의하고 사용하자
 
 function App() {
-  let list = [1, 2, 3];
+  // let number = 1; // 상태값 아님
+  const [number, setNumber] = useState(2); // React안에 hooks 라이브러리 상태값이 됨. (set + 카멜케이스로 쓴다)
+  const add = () => {
+    setNumber(number + 1); // 리엑트한테 number 값 변경할께 라고 요청
+    console.log('add', number);
+  };
+  // 랜더링 시점 = 상태값 변경
   return (
     <div>
       <div>
-        {list.map((n) => (
-          <h1>{n}</h1>
-        ))}
+        <h1>숫자 : {number}</h1>
+        <button onClick={add}>더하기</button>
+        <Sub />
       </div>
     </div>
   );
