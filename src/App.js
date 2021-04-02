a826eb45568fc54807d4afa1a07ba29c68d9b5c4
RichardKim <hiddeneventor@gmail.com>
2021-04-02 20:39:03 +0900

    useRef 예제

--- src/App.js
+++ src/App.js
@@ -1,55 +1,31 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-
-// 0. React 엔진 - 데이터변경감지해서 UI 그려주는!!
-// 1. 실행 과정 (index.html) - SPA, index.html에서 body만 바꾸는 기술, 페이지 이동이 아니다.
-// 2. JSX 문법 - javascript에서 html을 사용가능하게 하는 문법
-
-// 1) return시에 하나의 Dom만 리턴할 수 있다.
-// 2) 변수선언은 let혹은 const로만 해야한다.
-// 3) if사용 불가능 X => 삼항연산자 사용가능
-// 4) css 디자인 - 외부파일에 정의하고 사용하자
+import { createRef, useEffect, useMemo, useRef, useState } from 'react';
 
 function App() {
-  const [list, setList] = useState([1, 2, 3, 4]); // [1,2,3,4]
-  const [str, setStr] = useState('합계');
+  const [list, setList] = useState([
+    { id: 1, name: '홍길동' },
+    { id: 1, name: '테스트' },
+  ]);
 
-  // str변경하였는데, getAddResult가 호출이된다..
-  const getAddResult = () => {
-    let sum = 0;
-    list.forEach((i) => (sum = sum + i));
-    console.log('sum', sum);
-    return sum;
-  };
+  const myRef = useRef(null); // 1개밖에 안만들어진다.
 
-  // ✨ 해결방법 => useMemo함수 사용
-  const addResult = useMemo(() => getAddResult(), [list]); // 인자값1: [기억할 함수], 인자값2: [의존설정할 리스트]
+  // createRef() 동적으로 래퍼런스를 생성해주는 함수
+  const myRefs = Array.from({ length: list.length }).map(() => createRef());
 
   return (
     <div>
       <button
         onClick={() => {
-          setStr('변경');
+          // myRef.current.style.backgroundColor = 'red';
+          myRefs[0].current.style.backgroundColor = 'red';
         }}
       >
-        문자변경
+        색변경
       </button>
-
-      <button
-        onClick={() => {
-          setList([...list, 10]);
-        }}
-      >
-        리스트에 값추가
-      </button>
-      <div>
-        {list.map((i) => (
-          <h1>{i}</h1>
-        ))}
-      </div>
-      <div>
-        {str} : {getAddResult()}
-      </div>
+      <div ref={myRef}>박스</div>
+      {list.map((n, index) => (
+        <h1 ref={myRefs[index]}>{n.name}</h1>
+      ))}
     </div>
   );
 }
