21b5395c679ac4c68654ab60761adfb4b3f10a20
RichardKim <hiddeneventor@gmail.com>
2021-04-05 00:12:02 +0900

    뒤로가기 및 path 지정이동

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -5,7 +5,13 @@ const LoginPage = (props) => {
   console.log('LoginPage', props);
   console.log(props.match.params.id);
 
-  return <Login />;
+  return (
+    <div>
+      <button onClick={() => props.history.go(-1)}>뒤로가기</button>
+      <button onClick={() => props.history.push('/')}>Path지정 이동</button>
+      <Login />
+    </div>
+  );
 };
 
 export default LoginPage;
