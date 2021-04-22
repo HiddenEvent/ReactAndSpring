59f6ca6b73e0c38997e28ef77dcd78088f0fb46a
RichardKim <hiddeneventor@gmail.com>
2021-04-22 22:05:12 +0900

    데이터 전역으로 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route } from 'react-router';
 import Navigation from './components/Navigation';
 import ListPage from './page/ListPage';
@@ -5,11 +6,19 @@ import WritePage from './page/WritePage';
 
 // 글쓰기, 글삭제, 글목록보기
 function App() {
+  // 공통으로 사용할 필요가 있는 변수는 APP에서 선언하여 각 컴포넌트로 전달 (실제로 이렇게 사용하면 💩)
+  const [posts, setPosts] = useState([
+    { id: 1, title: '내용1' },
+    { id: 2, title: '내용2' },
+    { id: 3, title: '내용3' },
+    { id: 4, title: '내용4' },
+  ]);
+
   return (
     <div>
       <Navigation />
-      <Route path="/" exact={true} component={ListPage} />
-      <Route path="/Write" exact={true} component={WritePage} />
+      <Route path="/" exact={true} component={() => ListPage(posts)} />
+      <Route path="/Write" exact={true} component={() => WritePage(setPosts)} />
     </div>
   );
 }
--- src/page/ListPage.js
+++ src/page/ListPage.js
@@ -12,13 +12,6 @@ const StyledItemBoxDiv = styled.div`
 `;
 
 const ListPage = () => {
-  const [posts, setPosts] = useState([
-    { id: 1, title: '내용1' },
-    { id: 2, title: '내용2' },
-    { id: 3, title: '내용3' },
-    { id: 4, title: '내용4' },
-  ]);
-
   return (
     <div>
       <h1>리스트페이지</h1>
