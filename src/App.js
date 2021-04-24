5a12dc6388344c67b90171ac976cca9b88f2dbbf
RichardKim <hiddeneventor@gmail.com>
2021-04-24 22:42:25 +0900

    Redux 사용 store.js를 파일을 생성하여 상태값을 전역으로 사용 할 수 있도록 세팅하는 방법

--- src/App.js
+++ src/App.js
@@ -14,13 +14,7 @@ function App() {
     <div className="container">
       <h1>최상단 화면</h1>
 
-      {/* 탑에다 number 넘기기 */}
       <Top number={number} />
-
-      {/* 바텀에다가 setNumber를 바로 넘기지 말자.
-          1. 하위에 필요한 함수를 만들어 놓는다. 
-          2. 안그러면 number 속성을 하위 컴포넌트에서 마음대로 수정가능 하기 때문이다.
-       */}
       <Bottom addNumber={addNumber} />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import reducer from './store';
+import { Provider } from 'react-redux';
+
+// 리덕스가 들고있는 함수, reducer는 store.js에서 exprot default가 되어 있기때문에 바로 사용가능 하다.
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* store에 있는 모든 함수와 변수들을 전역으로 사용 가능해진다.  */}
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
