66ded540d2780329748374ea2c07cc3d9f6da316
RichardKim <hiddeneventor@gmail.com>
2021-04-24 22:45:19 +0900

    리덕스를 사용하기위해 기존 number값 초기화

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import Top from './components/Top';
 import Bottom from './components/Bottom';
 
 function App() {
-  const [number, setNumber] = useState(1);
-
-  const addNumber = () => {
-    setNumber(number + 1);
-  };
-
   return (
     <div className="container">
       <h1>최상단 화면</h1>
-
-      <Top number={number} />
-      <Bottom addNumber={addNumber} />
+      <Top />
+      <Bottom />
     </div>
   );
 }
--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,12 +1,11 @@
 import '../App.css';
 import React from 'react';
 
-const Bottom = (props) => {
-  const { addNumber } = props;
+const Bottom = () => {
   return (
     <div className="sub_container">
       <h1>Bootom</h1>
-      <button onClick={addNumber}>증가</button>
+      <button>증가</button>
     </div>
   );
 };
