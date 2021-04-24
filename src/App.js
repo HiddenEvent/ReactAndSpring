04cefaeeccb2628b62f961bd45fe7e2c96a48b43
RichardKim <hiddeneventor@gmail.com>
2021-04-24 22:28:04 +0900

    App에서 Bottom으로 number를 상태값을 변경할 함수를 넘기기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Bottom from './components/Bottom';
 function App() {
   const [number, setNumber] = useState(1);
 
+  const addNumber = () => {
+    setNumber(number + 1);
+  };
+
   return (
     <div className="container">
       <h1>최상단 화면</h1>
@@ -13,8 +17,11 @@ function App() {
       {/* 탑에다 number 넘기기 */}
       <Top number={number} />
 
-      {/* 바텀에다가 setNumber 넘기기 */}
-      <Bottom />
+      {/* 바텀에다가 setNumber를 바로 넘기지 말자.
+          1. 하위에 필요한 함수를 만들어 놓는다. 
+          2. 안그러면 number 속성을 하위 컴포넌트에서 마음대로 수정가능 하기 때문이다.
+       */}
+      <Bottom addNumber={addNumber} />
     </div>
   );
 }
