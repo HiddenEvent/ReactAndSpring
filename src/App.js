de9c1d5541c2656c2ac18ba69771c1c6c5b8c873
RichardKim <hiddeneventor@gmail.com>
2021-04-24 22:22:11 +0900

    구조분할 할당하여 하위 js 에서 데이터받아서 뿌려주기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Top from './components/Top';
 import Bottom from './components/Bottom';
 
 function App() {
+  const [number, setNumber] = useState(1);
+
   return (
     <div className="container">
       <h1>최상단 화면</h1>
-      <Top />
+
+      {/* 탑에다 number 넘기기 */}
+      <Top number={number} />
+
+      {/* 바텀에다가 setNumber 넘기기 */}
       <Bottom />
     </div>
   );
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,11 +1,14 @@
 import '../App.css';
 import React from 'react';
 
-const Top = () => {
+const Top = (props) => {
+  //구조분할 할당 하는게 좋음
+  const { number } = props;
+
   return (
     <div className="sub_container">
       <h1>TOP 화면</h1>
-      번호: 1
+      번호: {number}
     </div>
   );
 };
